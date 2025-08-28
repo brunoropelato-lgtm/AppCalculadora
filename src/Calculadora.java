b1be97f1f63aeabb168fda876740ba1ca2e699e2
brunoropelato-lgtm <bruno_ropelato@estudante.sesisenai.org.br>
2025-08-28 14:37:07 -0300

    Commit 2 - ADD subtração
    adicionando a função de subtração

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,5 +2,8 @@ public class Calculadora {
     public static int soma(int a, int b) {
         return a + b;
     }
+    public static int subtração(int a, int b) {
+        return a + b;
+    }
     
 }
